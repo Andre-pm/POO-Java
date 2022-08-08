4acc2c41a2fc7c6be535e0c3994e5546f5142f48
André Pereira Martins <53614466+Andre-pm@users.noreply.github.com>
2022-08-08 20:56:43 -0300

    adicionando os arquivos do projeto

---
+++ aula01/src/ifsc/PrimeiroProgramaJava.java
@@ -0,0 +1,38 @@
+package ifsc;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PrimeiroProgramaJava {
+
+	public static void main(String[] args) {
+		List<String> lista = new ArrayList<>();
+		lista.add("Bruna");
+		lista.add("Luana");
+		lista.add("André");
+		
+		System.out.println("Tamanho da lista: " + lista.size());
+		System.out.println("Lista está vázia: " + lista.isEmpty());
+		System.out.println("Conteúdo da lista: " + lista.toString());
+		
+//		Forma indo por item:
+		for(String item : lista ) {
+			System.out.println(item);
+		}
+		
+//		Forma indo pelo index:
+		
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i));
+		}
+		
+		
+		int contador = 0;
+		while(contador < lista.size()) {
+			System.out.println(lista.get(contador));
+			contador++;
+		}
+	}
+
+}
